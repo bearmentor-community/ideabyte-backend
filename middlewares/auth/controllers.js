634fd915ac9e0b3cc7dda0fafd6341b7f15b0779
M Haidar Hanif <git@mhaidarhanif.com>
2019-03-11 20:37:00 +0700

    Enhance with isUserExist

--- middlewares/auth/controllers.js
+++ middlewares/auth/controllers.js
@@ -1,5 +1,7 @@
 const helpers = require('../../helpers')
 
+const User = require('../users/model')
+
 const authControllers = {
   //////////////////////////////////////////////////////////////////////////////
   // GET TOKEN
@@ -8,14 +10,16 @@ const authControllers = {
       // Get the token in headers of Authorization
       // Authorization: Bearer {this_is_the_token}
       const token = req.headers.authorization.split(' ')[1]
+      const decoded = await helpers.verifyToken(token)
 
       // the token will be saved somewhere soon
       // in testing, you can save it in Postman configuration
       // in frontend app, you can save it in Redux store/state
 
-      // we put the token in request object
-      // so it can be accessed by other middleware
+      // we put the token and decoded in request object
+      // so it can be accessed by other middlewares
       req.token = token
+      req.decoded = decoded
 
       // go to next middleware
       next()
@@ -23,6 +27,24 @@ const authControllers = {
       // If failed to get the token
       res.send({
         message: 'Token is not exist in headers of Authorization'
+      })
+    }
+  },
+
+  //////////////////////////////////////////////////////////////////////////////
+  // CHECK IF USER ALREADY EXIST
+  isUserExist: async (req, res, next) => {
+    const user = User.findOne(
+      { email: req.params.email },
+      { salt: 0, password: 0 }
+    )
+
+    // if user does not exist, you can continue
+    if (!user) {
+      next()
+    } else {
+      res.send({
+        message: 'User is already exist with that email!'
       })
     }
   }
